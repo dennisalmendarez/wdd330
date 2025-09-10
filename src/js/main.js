1058aafdbb16fa4027d065a337930909d07fb439
jmostert1 <joshuamostert9@gmail.com>
2025-09-10 15:11:30 +0200

    Fix import statement in main.js to include ProductData

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-import ProductList from "./ProductList.mjs";import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
 
 const dataSource = new ProductData("tents");
