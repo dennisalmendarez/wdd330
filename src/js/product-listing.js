7065c9572c4b6dfe1f77bf9197b062986e6a6bd9
Bhekimpilo <ncu22004@byui.edu>
2025-09-22 19:40:34 +0200

    Adding product categories

---
+++ src/js/product-listing.js
@@ -0,0 +1,7 @@
+import ProductList from "./ProductList.mjs";
+import ProductData from "./ProductData.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+
+// Load header and footer on page load
+loadHeaderFooter();
+
